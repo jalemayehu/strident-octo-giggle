87167497ca251ded39e2f0623784d2959eb2caa5
git <git@malc.malc.net>
2015-11-30 04:04:03 -0500

    licensefile init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,36 @@
-# Class: licensefile
+# == Class: licensefile
 #
+# Full description of class licensefile here.
 #
+# === Parameters
+#
+# None.
+#
+# === Variables
+#
+# None.
+#
+# === Authors
+#
+# Author Name malc@mail.com
+#
+# === Copyright
+#
+# Copyright 2015 Malcolm Badley.
+#
+
 class licensefile {
-  # TODO
+#ensure directories exist
+file { [ '/path',
+         '/path/to', ]:
+          ensure => directory,
+  } ->
+
+#ensure license file exists
+file { '/path/to/file.lic':
+                unless => '/bin/ls /path/to/file.lic.installed',
+                source => "puppet:///modules/strident-octo-giggle/file.lic",
+                ensure => present,
+  }
+
 }
